1b89a4332996af99f7d185c14d280e74dde41f65
Tom O'Brien <tom@lumi.news>
2016-11-29 20:00:58 +0000

    Remaining exercises

--- Party.hs
+++ Party.hs
@@ -1,6 +1,7 @@
 module Party where
 
 import Data.Tree
+import Data.List
 import Employee
 
 
@@ -17,3 +18,40 @@ moreFun :: GuestList -> GuestList -> GuestList
 moreFun gl1@(GL _ fun1) gl2@(GL _ fun2)
     | fun1 >= fun2 = gl1
     | otherwise    = gl2
+
+
+-- Exercise 2
+
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node label forest) = f label $ map (treeFold f) forest
+
+
+-- Exercise 3
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss guestListPairs = (glCons boss $ mconcat $ map snd guestListPairs
+                                , mconcat $ map fst guestListPairs)
+
+
+-- Exercise 4
+
+maxFun :: Tree Employee -> GuestList
+-- maxFun company@(Node {rootLabel = boss}) = uncurry moreFun $ treeFold nextLevel company
+maxFun company = uncurry moreFun $ treeFold nextLevel company
+
+
+-- Exercise 5
+
+instance Ord Employee where
+    (<=) (Emp name1 _) (Emp name2 _) = name1 <= name2
+
+getName :: Employee -> String
+getName (Emp name _) = name
+
+asString :: GuestList -> String
+asString (GL employees totalFun) = "Total fun: " ++ show totalFun ++ "\n" ++
+                                   unlines (sort (map getName employees))
+
+main :: IO ()
+main = readFile "company.txt"
+       >>= putStrLn . asString . maxFun . read
